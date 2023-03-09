cea1984f6dfb875822db9f51182c907ed55f46c9
@SeanEmile <seanemile.se@protonmail.com>
2023-03-09 10:26:35 -0700

    Added yul challanges

---
+++ doc/solc_doc/yul.md
@@ -0,0 +1,5 @@
+# Yul (Inter midiate language).
+1. [Code Challange](Calldata.sol)
+    - Write a contract with a function "add()" that takes in two uint256 inputs adds them and returns the result impliment the body of the function in yul. 
+
+
---
+++ src/solc_doc/yul/Calldata.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+/**
+  * @dev Abi decoder in solidity.
+  */
+
+pragma solidity 0.8.19;
+
+contract Calldata {
+    // function add(uint256 _a, uint256 _b) public view returns (uint256 result) {
+    //     assembly {
+    //         let a := mload(0x40)
+    //         let b := add(a, 32)
+    //         calldatacopy(a, 4, 32)
+    //         calldatacopy(b, add(4, 32), 32)
+    //         result := add(mload(a), mload(b))
+    //     }
+    // }
+}
